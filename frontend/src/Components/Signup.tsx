7c69fbef0616d0e2f55491f5fa110151444005df
Gaurav Arsod <gaarsod14@gmail.com>
2022-10-17 23:28:59 +0530

    Created signup page UI

--- frontend/src/Components/Signup.tsx
+++ frontend/src/Components/Signup.tsx
@@ -1,26 +1,81 @@
 import { useState } from "react";
 import TextField from '@mui/material/TextField';
 import style from "../Styles/Signup.module.css";
-
+import MenuItem from '@mui/material/MenuItem';
+import {FormControl, InputLabel, Select, SelectChangeEvent} from "@mui/material";
 
 export const Signup = () => {
+    const [age, setAge] = useState<string>('');
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setAge(event.target.value);
+    };
+
     return (
         <div id={style.signupContainer}>
             <form action="">
                 <TextField
                     fullWidth
                     id="standard-error-helper-text"
-                    label="E-mail"
-                    //   helperText="Incorrect entry."
+                    label="Name"
                     variant="standard"
-                //   color="error"
-                // onChange={(e) => {
-                //     setValidate(e.target.value);
-                // }}
+                    name="name"
+                />
+                <TextField
+                    fullWidth
+                    id="standard-error-helper-text"
+                    label="E-mail"
+                    variant="standard"
+                    name="email"
+                />
+                <TextField
+                    fullWidth
+                    id="standard-error-helper-text"
+                    label="Phone Number"
+                    name="phoneNumber"
+                    variant="standard"
+                />
+                <TextField
+                    fullWidth
+                    id="standard-error-helper-text"
+                    label="City"
+                    name="city"
+                    variant="standard"
+                />
+                <FormControl fullWidth variant="standard" >
+                    <InputLabel id="demo-simple-select-standard-label">State</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-standard-label"
+                        id="demo-simple-select-standard"
+                        value={age}
+                        onChange={handleChange}
+                        label="Age"
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value="Andhra Pradesh">Andhra Pradesh</MenuItem>
+                        <MenuItem value="Delhi">Delhi</MenuItem>
+                        <MenuItem value="Gujrat">Gujrat</MenuItem>
+                        <MenuItem value="Maharashtra">Maharashtra</MenuItem>
+                        <MenuItem value="Karnataka">Karnataka</MenuItem>
+                        <MenuItem value="Madhya Pradesh">Madhya Pradesh</MenuItem>
+                        <MenuItem value="Kerala">Kerala</MenuItem>
+                        <MenuItem value="Telangana">Telangana</MenuItem>
+                    </Select>
+                </FormControl>
+                
+
+                <TextField
+                    fullWidth
+                    id="standard-error-helper-text"
+                    label="Password"
+                    variant="standard"
+                    name="password"
                 />
                 <TextField
                     id="standard-error-helper-text"
-                    label="Password"
+                    label="Confirm Password"
                     //   helperText="Incorrect entry."
                     variant="standard"
                 //   color="error"
@@ -29,7 +84,7 @@ export const Signup = () => {
                 // }}
                 />
 
-                <button>Login</button>
+                <button>Sign Up</button>
             </form>
         </div>
     )
